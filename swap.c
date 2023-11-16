5930edb4128f3f8b40902a9ad410bf9ef097ea7c
AlexDelei <alexdelei125@gmail.com>
2023-11-16 11:29:56 -0800

    Monty language

--- swap.c
+++ swap.c
@@ -2,27 +2,24 @@
 
 /**
  * swap - swaps the top two elements of the stack
- * @stack: pointer to the stack
- * @line_number: line number of the opcode in the Monty file
+ * @stack: double pointer to the beginning of the stack
+ * @line_number: current line number in the Monty bytecodes file
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-	stack_t *top = *stack;
-	stack_t *next_node = top->next;
+	stack_t *temp;
 
-	if (*stack == NULL || (*stack)->next == NULL)
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	top->next = next_node->next;
-	if (next_node->next != NULL)
-	{
-		next_node->next->prev = top;
-	}
-	next_node->prev = NULL;
-	next_node->next = top;
-	top->prev = next_node;
-
-	*stack = next_node;
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = *stack;
+	temp->prev = NULL;
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	*stack = temp;
 }
